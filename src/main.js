62d0d2978523e685c85abc5e477d40000a57fc1f
JacobStrayer <gaseousfob@gmail.com>
2024-10-01 12:26:58 -0400

    Vite config base folder

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
-import { pinia } from './stores'
 
 const app = createApp(App)
 
+app.use(createPinia())
 app.use(router)
-app.use(pinia)
 
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/JacobStrayer.github.io/',
     plugins: [vue(), vueJsx()],
     resolve: {
         alias: {
