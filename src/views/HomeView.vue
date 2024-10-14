7c7e8eabdc99ada892dc2ccbe56749e4bc73347f
JacobStrayer <gaseousfob@gmail.com>
2024-10-14 19:00:06 -0400

    Showing the boids...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,14 @@
     <div class="home">
         <HeroSection />
         <SkillGrid />
+        <BoidsField />
     </div>
 </template>
 
 <script setup>
 import HeroSection from '../components/HeroSection.vue'
 import SkillGrid from '../components/SkillGrid.vue'
+import BoidsField from '@/components/BoidsField.vue';
 </script>
 
 <style scoped>
